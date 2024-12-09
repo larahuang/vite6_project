784315f1f7fcf39fa45b74087ddaf21745a1330e
lara huang <lara1105huang@gmail.com>
2024-12-09 21:42:06 +0800

    安裝tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import router from './router/router';
 
 import App from './App.vue'
-import '@/assets/scss/styles.scss';
+import './style.css' 
+//import '@/assets/scss/styles.scss';
 const app = createApp(App)
     app.use(router)
 app.mount('#app')
